1992bc7901703c30f189a6222610b6a0bde53b20
Adriano Eduardo Trentin Junior <adrianoeduardotrentin@hotmail.com>
2023-08-04 17:59:23 -0300

    Commit 02
    [UPD] productsFunction.ts -> uncomment lines of crud dybamo db.

--- lambda/products/productsFunction.ts
+++ lambda/products/productsFunction.ts
@@ -10,7 +10,8 @@ export interface Product {
     model: string
 }
 
-const a = new DocumentClient()
+const ddbClient = new DocumentClient();
+const productDdb = process.env.PRODUCTS_DDB!;
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
  
@@ -35,75 +36,77 @@ export async function handler(event: APIGatewayProxyEvent, context: Context): Pr
         }
 
         if(method === 'POST'){
-            console.log(`POST /products`)
+            const product = JSON.parse(event.body!) as Product
 
+            const createdProduct = await createProduct(product);
+            
             return {
                 statusCode: 201,
-                body: JSON.stringify('ola'),
-            }
+                body: JSON.stringify(createdProduct),
+            }; 
         }
     }
     
-    // if(event.resource === '/products/{id}'){
-    //     const productId = event.pathParameters!.id as string;
+    if(event.resource === '/products/{id}'){
+        const productId = event.pathParameters!.id as string;
 
-    //     if(method === 'GET'){
-    //         console.log(`GET /products/{id}`);
+        if(method === 'GET'){
+            console.log(`GET /products/{id}`);
 
-    //         try {
-    //             const product = await getProductById(productId);
+            try {
+                const product = await getProductById(productId);
 
-    //             return {
-    //                 statusCode: 200,
-    //                 body: JSON.stringify(product)
-    //             }                
-    //         } catch (error) {
-    //             console.error((<Error>error).message)
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(product)
+                }                
+            } catch (error) {
+                console.error((<Error>error).message)
 
-    //             return {
-    //                 statusCode: 404,
-    //                 body: (<Error>error).message
-    //             }
-    //         }
+                return {
+                    statusCode: 404,
+                    body: (<Error>error).message
+                }
+            }
 
-    //     }
+        }
 
-    //     if(method === 'DELETE'){
-    //         console.log(`DELETE /products/${productId}`);
+        if(method === 'DELETE'){
+            console.log(`DELETE /products/${productId}`);
 
-    //         try {
-    //             const product = await deleteProductById(productId);
-    //             return{
-    //                 statusCode: 200,
-    //                 body: JSON.stringify(product),
-    //             }
-    //         } catch (error) {
-    //             console.error((<Error>error).message);
+            try {
+                const product = await deleteProductById(productId);
+                return{
+                    statusCode: 200,
+                    body: JSON.stringify(product),
+                }
+            } catch (error) {
+                console.error((<Error>error).message);
 
-    //             return {
-    //                 statusCode: 404,
-    //                 body: (<Error>error).message,
-    //             }
-    //         }
-    //     }
+                return {
+                    statusCode: 404,
+                    body: (<Error>error).message,
+                }
+            }
+        }
         
-    //     if(method === 'PUT'){
-    //         const product = JSON.parse(event.body!) as Product;
+        if(method === 'PUT'){
+            const product = JSON.parse(event.body!) as Product;
             
-    //         try {
-    //             const productUpdated = await updateProduct(productId, product);
-    //             return { 
-    //                 statusCode: 200, 
-    //                 body: JSON.stringify(productUpdated)
-    //             }
-    //         } catch (ConditionalCheckFailedException) {
-    //             return {
-    //                 statusCode: 404, 
-    //                 body: 'Product not found'
-    //             }
-    //         }
-    //     }
-    // }
+            try {
+                const productUpdated = await updateProduct(productId, product);
+                return { 
+                    statusCode: 200, 
+                    body: JSON.stringify(productUpdated)
+                }
+            } catch (ConditionalCheckFailedException) {
+                return {
+                    statusCode: 404, 
+                    body: 'Product not found'
+                }
+            }
+        }
+    }
 
     return {
         statusCode: 400,
@@ -114,74 +117,74 @@ export async function handler(event: APIGatewayProxyEvent, context: Context): Pr
         LambdaRequestId: lambdaRequestId,
         }),
     };
-
-    async function getAllProducts(): Promise<Product[]> {
-        // NAO FAÇA ISSO
-        const data = await a.scan({
-            TableName: 'products',
-        }).promise()
-    
-        return data.Items as Product[];
-    }
 }
 
-// async function updateProduct(productId: string, product: Product) {
-//     const data = await ddbClient.update({
-//         TableName: productDdb,
-//         Key: {
-//             id: productId,
-//         },
-//         ConditionExpression: 'attribute_exists(id)',
-//         ReturnValues: 'UPDATED_NEW',
-//         UpdateExpression: 'set productName = :n, code = :c, price = :p, model = :m',
-//         ExpressionAttributeValues: {
-//             ':n': product.productName,
-//             ':c': product.code,
-//             ':p': product.price,
-//             ':m': product.model,
-//         }
-//     }).promise();
+async function updateProduct(productId: string, product: Product) {
+    const data = await ddbClient.update({
+        TableName: productDdb,
+        Key: {
+            id: productId,
+        },
+        ConditionExpression: 'attribute_exists(id)',
+        ReturnValues: 'UPDATED_NEW',
+        UpdateExpression: 'set productName = :n, code = :c, price = :p, model = :m',
+        ExpressionAttributeValues: {
+            ':n': product.productName,
+            ':c': product.code,
+            ':p': product.price,
+            ':m': product.model,
+        }
+    }).promise();
 
-//     data.Attributes!.id = productId;
-//     return data.Attributes as Product;
-// }
+    data.Attributes!.id = productId;
+    return data.Attributes as Product;
+}
 
-// async function createProduct(product: Product): Promise<Product> {
-//     product.id = uuid();
-//     ddbClient.put({
-//         TableName: productDdb,
-//         Item: product,    
-//     }).promise();
+async function createProduct(product: Product): Promise<Product> {
+    product.id = uuid();
+    ddbClient.put({
+        TableName: productDdb,
+        Item: product,    
+    }).promise();
 
-//     return product;
-// }
+    return product;
+}
 
-// async function deleteProductById(productId: string): Promise<Product> {
-//     const data = await ddbClient.delete({
-//         TableName: productDdb,
-//         Key: {
-//             id: productId,
-//         },
-//         ReturnValues: 'ALL_OLD',
-//     }).promise()
+async function deleteProductById(productId: string): Promise<Product> {
+    const data = await ddbClient.delete({
+        TableName: productDdb,
+        Key: {
+            id: productId,
+        },
+        ReturnValues: 'ALL_OLD',
+    }).promise()
 
-//     if (data.Attributes)
-//         return data.Attributes as Product;
-//     else 
-//         throw new Error('Product nor found');
-// }
+    if (data.Attributes)
+        return data.Attributes as Product;
+    else 
+        throw new Error('Product nor found');
+}
 
-// async function getProductById(productId: string) {
-//     const data = await ddbClient.get({
-//         TableName: productDdb,
-//         Key: {
-//             id: productId,
-//         }
-//     }).promise()
+async function getProductById(productId: string) {
+    const data = await ddbClient.get({
+        TableName: productDdb,
+        Key: {
+            id: productId,
+        }
+    }).promise()
 
-//     if(data.Item)
-//         return data.Item as Product;
-//     else 
-//         throw new Error('Product not found');
-// }
+    if(data.Item)
+        return data.Item as Product;
+    else 
+        throw new Error('Product not found');
+}
+
+async function getAllProducts(): Promise<Product[]> {
+    // NAO FAÇA ISSO
+    const data = await ddbClient.scan({
+        TableName: 'products',
+    }).promise()
+
+    return data.Items as Product[];
+}
 
